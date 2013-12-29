4420be4703e9b4d17407f6db1b9defb3a8eb4a45
Marco Vito Moscaritolo <marco@agavee.com>
2013-12-29 22:46:36 +0100

    Update styling and removed SkelJS

--- source/js/init.js
+++ source/js/init.js
@@ -4,6 +4,7 @@
   html5up.net | @n33co
   Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+/*
 skel.init(
   {
     prefix: '/css/style',
@@ -89,7 +90,7 @@ skel.init(
     }).trigger('resize');
   });
 }(jQuery));
-
+*/
 (function ($) {
   var qualifyURL = function (url) {
     var a = document.createElement('a');
