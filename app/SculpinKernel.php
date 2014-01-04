48a40c160aa0475a249f75425f5d65aa81245f2b
Marco Vito Moscaritolo <marco@agavee.com>
2014-01-04 17:40:21 +0100

    Added some new bundles to manage content redirects and some editor helper.

--- app/SculpinKernel.php
+++ app/SculpinKernel.php
@@ -4,6 +4,10 @@ class SculpinKernel extends \Sculpin\Bundle\SculpinBundle\HttpKernel\AbstractKer
 {
     protected function getAdditionalSculpinBundles()
     {
-        return array('Mavimo\Sculpin\Bundle\ProjectsBundle\SculpinProjectsBundle');
+        return array(
+           'Mavimo\Sculpin\Bundle\ProjectsBundle\SculpinProjectsBundle',
+           'Mavimo\Sculpin\Bundle\EditorBundle\SculpinEditorBundle',
+           'Mavimo\Sculpin\Bundle\RedirectBundle\SculpinRedirectBundle'
+        );
     }
 }
